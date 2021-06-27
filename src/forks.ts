ab26786465df24cc69ad5e6f107bc7d6ece2fa8a
Aniket-Engg <aniketengg.225@gmail.com>
2021-06-27 14:11:51 +0530

    forks detail added

---
+++ src/forks.ts
@@ -0,0 +1,46 @@
+export default {
+    chainstart: {
+        block: 0,
+        year: 2015
+    },
+    homestead: {
+        block: 1150000,
+        year: 2016
+    },
+    dao: {
+        block: 1920000,
+        year: 2016
+    },
+    tangerineWhistle: {
+        block: 2463000,
+        year: 2016
+    },
+    spuriousDragon: {
+        block: 2675000,
+        year: 2016
+    },
+    byzantium: {
+        block: 4370000,
+        year: 2017
+    },
+    constantinople: {
+        block: 7280000,
+        year: 2019
+    },
+    petersburg: {
+        block: 7280000,
+        year: 2019
+    },
+    istanbul: {
+        block: 9069000,
+        year: 2019
+    },
+    muirGlacier: {
+        block: 9200000,
+        year: 2020
+    },
+    berlin: {
+        block: 12244000,
+        year: 2021
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,3 @@
-export const test = (name: string) => `Hello ${name}`; 
+import forks from './forks'
+
+console.log(forks)
